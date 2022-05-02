4c6831348e3a4cad48ad5897604273afc6e8c629
lhcamposs <lhcampossoares@gmail.com>
2022-05-02 18:16:33 -0300

    Projeto de html e css3 que foi feito nas aulas

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="PT">
+<head>
+    <title>*Meu primeiro Site em PHP!*</title>
+</head>
+<body>
+    <?php
+        for($i = 0; $i < 10; $i++ ){
+            print("Linha número " . $i . "<br />");
+         }
+    ?>
+</body>
+</html>
